e588271f8fa8253c4cf065369081dd77929f693e
deepakAppstrail <deepak.paliwal@appstrail.com>
2024-07-03 17:32:04 +0530

    fix: issues

--- src/movie-get/movie-get.service.ts
+++ src/movie-get/movie-get.service.ts
@@ -5,46 +5,58 @@ import { getSignedUrl } from 'src/common/firebaseHelpers';
 
 @Injectable()
 export class MovieGetService {
-
-    async getAllMovies() {
-        try{
-            let movieData =  await getData(MovieCollection);
-            let response = []
-            for(let i of movieData) {
-                let url = ""
-                if(i.poster) {
-                    url = await getSignedUrl(i.poster)
-                }
-                response.push({
-                    ...i,
-                    poster: url
-                })
-            }
-            return response
-          }catch (err) {
-            throw new HttpException(
-              err.message,
-              err.status || HttpStatus.INTERNAL_SERVER_ERROR,
-            );
-          }
+  async getAllMovies() {
+    try {
+      let movieData = await getData(MovieCollection);
+      let response = [];
+      for (let i of movieData) {
+        let url = '';
+        if (i.poster) {
+          url = await getSignedUrl(i.poster);
+        }
+        response.push({
+          ...i,
+          poster: url,
+        });
+      }
+      return response;
+    } catch (err) {
+      throw new HttpException(
+        err.message,
+        err.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+  }
 
-    async getMovieDataUsingId(id: string) {
-        try{
-            const movieData = await getDataById(MovieCollection, id);
-            if(movieData.poster){
-                movieData.poster = await getSignedUrl(movieData.poster);
-            }
-            const comments = await getDataById(CommentsCollection, id);
-            return {
-                movieData,
-                ...comments
-            }
-          }catch (err) {
-            throw new HttpException(
-              err.message,
-              err.status || HttpStatus.INTERNAL_SERVER_ERROR,
-            );
-          }
+  async getMovieDataUsingId(id: string) {
+    try {
+      const movieData = await getDataById(MovieCollection, id);
+      console.log(movieData);
+      if (movieData.poster) {
+        movieData.poster = await getSignedUrl(movieData.poster);
+      }
+      let comments;
+      try {
+        comments = await getDataById(CommentsCollection, id);
+      } catch (err) {
+        console.log(err);
+      }
+
+      if (comments) {
+        return {
+          movieData,
+          ...comments,
+        };
+      }
+      return {
+        movieData,
+        comments: [],
+      };
+    } catch (err) {
+      throw new HttpException(
+        err.message,
+        err.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+  }
 }
